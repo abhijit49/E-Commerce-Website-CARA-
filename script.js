ba1f68b0da2cdabb21efbbefa38a9d6632ea6bd9
Abhijit Bhandary <91731000+abhijit812@users.noreply.github.com>
2022-10-07 18:57:24 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click',() =>{
+        nav.classList.add('active');
+    })
+}
