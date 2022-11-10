5e917e9b60d63c80e209206fcd53646d7b327dc4
singhkaran202 <103121051@nitt.edu>
2022-11-10 11:20:06 +0530

    Cart page is made dynamic

--- script.js
+++ script.js
@@ -24,3 +24,46 @@ window.addEventListener("scroll", () => {
         toTop.classList.remove("active");
     }
 })
+
+var removeCartItembtn = document.getElementsByClassName('remove')
+console.log(removeCartItembtn)
+for(var i=0; i<removeCartItembtn.length; i++){
+    var button = removeCartItembtn[i]
+    button.addEventListener('click', function(event){
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.parentElement.parentElement.remove()
+        updateCartTotal()
+    })
+}
+
+var quantityInputs = document.getElementsByClassName('quantity')
+for(var i=0; i<quantityInputs.length; i++){
+    var input = quantityInputs[i]
+    input.addEventListener('change', quantitychanged)
+}
+
+function quantitychanged(event){
+    var input = event.target
+    if(isNaN(input.value) || input.value <= 0){
+     input.value = 1
+    }
+    updateCartTotal()
+}
+
+function updateCartTotal(){
+    var cartRows = document.getElementsByClassName('cart-row')
+    var total = 0;
+    var total1 = 0;
+    for(i=0; i<cartRows.length; i++){
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('price')[0]
+        var quantityElement = cartRow.getElementsByClassName('quantity')[0]
+        var price = parseFloat(priceElement.innerText.replace('$',''))
+        var quantity = quantityElement.value
+        total = total+(price * quantity)
+        }
+        total1 = total
+        total = total+35
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+    document.getElementsByClassName('cart-subtotal-price')[0].innerText = '$'+total1
+}
