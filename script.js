3486420af2dff2e111039b03b2f6ad0c321a59fa
singhkaran202 <103121051@nitt.edu>
2022-11-11 18:36:29 +0530

    Cart page is made fully functional

--- script.js
+++ script.js
@@ -24,3 +24,96 @@ window.addEventListener("scroll", () => {
         toTop.classList.remove("active");
     }
 })
+
+var removeCartItembtn = document.getElementsByClassName('remove')
+console.log(removeCartItembtn)
+for(var i=0; i<removeCartItembtn.length; i++){
+    var button = removeCartItembtn[i]
+    button.addEventListener('click', function(event){
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.parentElement.parentElement.remove()
+        updateCartTotal()
+    })
+}
+
+var quantityInputs = document.getElementsByClassName('quantity')
+for(var i=0; i<quantityInputs.length; i++){
+    var input = quantityInputs[i]
+    input.addEventListener('change', quantitychanged)
+}
+
+var cartbtn = document.getElementsByClassName('btn-cart')
+for(var i=0; i<cartbtn.length; i++){
+    var input = cartbtn[i]
+    input.addEventListener('click', addToCartClicked)  
+}
+
+
+function quantitychanged(event){
+    var input = event.target
+    if(isNaN(input.value) || input.value <= 0){
+     input.value = 1
+    }
+    updateCartTotal()
+}
+
+function updateCartTotal(){
+    var cartRows = document.getElementsByClassName('cart-row')
+    var total = 0;
+    var total1 = 0;
+    for(i=0; i<cartRows.length; i++){
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('price')[0]
+        var quantityElement = cartRow.getElementsByClassName('quantity')[0]
+        var price = parseFloat(priceElement.innerText.replace('$',''))
+        var quantity = quantityElement.value
+        total = total+(price * quantity)
+        }
+        total1 = total
+        total = total+35
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+    document.getElementsByClassName('cart-subtotal-price')[0].innerText = '$'+total1
+}
+function addToCartClicked(event){
+    
+    var btnn= event.target
+    var pdt =btnn.parentElement
+    var pdtname = pdt.getElementsByClassName('name')[0].innerText
+    var pdtprice = pdt.getElementsByClassName('cost')[0].innerText
+    var img = pdt.getElementsByClassName('img')[0].src
+    console.log(pdtname, pdtprice, img)
+    addItemToCart(pdtname, pdtprice, img)
+    updateCartTotal()
+}
+function addItemToCart(pdtname, pdtprice, img){
+    var cartRow= document.createElement('tr')
+
+    var cartItems = document.getElementsByClassName('table')[0]
+
+    var cartRowContents =`
+        
+          <td>
+            <div class="cart-info">
+              <img src="${img}" width="120" hieght="120" alt="Tshirt" />
+              <div>
+                <p>${pdtname}</p>
+                <small>Price: ${pdtprice}</small>
+                <br />
+                <button class="remove">Remove</a></button>
+              </div>
+            </div>
+          </td>
+          <td><input class="quantity" type="number" value="1"  width="50px" height="30px" /></td>
+          <td class="price">${pdtprice}</td>
+    `
+    cartRow.innerHTML = cartRowContents
+    cartRow.classList.add('cart-row')
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('remove')[0].addEventListener('click', function(event){
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.parentElement.parentElement.remove()
+        updateCartTotal()
+    })
+        cartRow.getElementsByClassName('quantity')[0].addEventListener('change',quantitychanged)
+
+    }
